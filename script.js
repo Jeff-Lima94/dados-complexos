7fe998615da4b30f05634661469a1826249594f6
Jeff-Lima94 <jeffersonlima10.jls@gmail.com>
2022-08-05 10:30:30 -0300

    first-commit

---
+++ script.js
@@ -0,0 +1,23 @@
+// Crie um array que receba 5 itens e exiba no console.
+let dados = ["Futebol","Games","Tecnologia","Musica","Filme" ];
+console.log(dados);
+
+// Utilize um método para adicionar um nome ao inicio do array.
+dados.unshift('Rock');
+console.log(dados);
+
+// Utilize um método para remover o último nome do array.
+dados.pop();
+console.log(dados);
+
+// Utilize um método para adicionar dois nomes ao fim do array.
+dados.push('Series','Anime');
+console.log(dados);
+
+// Utilize um método para remover o primeiro nome do array.
+dados.shift();
+console.log(dados);
+
+
+let number = [7,5,6,3,8,9,2,1,4]
+console.log(number.sort());
